e7e6b9a2668f690a33729042c7309ca199e2e66d
ZiiHenggg <phangziheng@gmail.com>
2025-06-28 13:50:07 +0800

    okok

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -1,19 +1,22 @@
 <?php
 namespace App\Repository;
-use App\Entity\Review; 
 
-interface ReviewRepository {
-    
+use App\Entity\Review;
+
+interface ReviewRepository
+{
     public function addReview(Review $review): void;
-    
+
     public function getReview(int $reviewId): ?Review;
 
     public function getReviewsForReviewee(int $studentId): array;
-    
+
     public function getReviewsByReviewer(int $studentId): array;
 
     public function hasUserReviewed(int $reviewerId, int $revieweeId, int $groupId): bool;
 
+    public function resolveGroupMembersId(int $reviewerId, int $revieweeId, int $groupId): int;
+
     // public function getAllReviews(): array;
 }
-?>
+
