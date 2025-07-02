2f1db3f842c821d2f72ee5710013029ca9a108c9
ZiiHenggg <phangziheng@gmail.com>
2025-07-02 17:50:13 +0800

    test

--- tests/UI/tests/login.spec.ts
+++ tests/UI/tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Login page loads', async ({ page }) => {
-  await page.goto('http://web/login.php');
+  await page.goto('http://localhost:8080/login.php');
   await expect(page).toHaveTitle(/login/i); // Adjust to match your login page title
 });
