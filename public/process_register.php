88c5c2b60c02395290dff475d2b021605b33825b
hhuiminc <hhuiminc@gmail.com>
2025-07-04 19:19:11 +0800

    ui test

--- public/process_register.php
+++ public/process_register.php
@@ -26,7 +26,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             SessionManager::setRegisterStep('otp');
             SessionManager::setRegisterMessage('Mocked OTP step in CI');
             SessionManager::setRegisterMessageType('success');
-            SessionManager::resetRegisterFlow();
             SessionManager::setRegisterSuccess(true);
             header('Location: register.php');
             exit;
