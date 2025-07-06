1d964596064ac0269dac812027e09b029a031334
hhuiminc <hhuiminc@gmail.com>
2025-07-06 14:48:21 +0800

    Update StudentPageController.php

--- src/Boundary/StudentPageController.php
+++ src/Boundary/StudentPageController.php
@@ -2,16 +2,19 @@
 namespace App\Boundary;
 
 use App\Control\StudentControl;
+use App\Control\ReviewControl;
 use App\Entity\Student;
 use DivineOmega\PasswordExposed\Enums\PasswordStatus;
 
 class StudentPageController
 {
     private StudentControl $studentControl;
+    private ReviewControl $reviewControl;
 
-    public function __construct(StudentControl $studentControl)
+    public function __construct(StudentControl $studentControl, ReviewControl $reviewControl)
     {
         $this->studentControl = $studentControl;
+        $this->reviewControl = $reviewControl;
     }
 
     public function validatePassword(string $password): ?string
